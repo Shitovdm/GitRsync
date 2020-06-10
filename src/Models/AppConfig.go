e2ca6230d3ff98eded53e02a8cf98b2dff7277a9
Dmitry Shitov <shitov.dm@gmail.com>
2020-06-10 12:17:07 +0300

    Added commits overriding config structs.

--- src/Models/AppConfig.go
+++ src/Models/AppConfig.go
@@ -1,11 +1,27 @@
 package Models
 
 type AppConfig struct {
-	Common Common `json:"common"`
+	Common            Common            `json:"common"`
+	CommitsOverriding CommitsOverriding `json:"commits_overriding"`
 }
 
 type Common struct {
 	RecentCommitsShown int `json:"recent_commits_shown"`
+}
+
+type CommitsOverriding struct {
+	State           bool             `json:"state"`
+	CommittersRules []CommittersRule `json:"committers_rules"`
+}
+
+type CommittersRule struct {
+	Old GitUser `json:"old"`
+	New GitUser `json:"new"`
+}
+
+type GitUser struct {
+	Username string `json:"username"`
+	Email    string `json:"email"`
 }
 
 type SaveSettingsRequest struct {
